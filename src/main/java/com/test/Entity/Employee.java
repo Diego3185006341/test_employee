6fbd835806aba2591cec97dd65ca9b0fe9741e96
Diego3185006341 <90737409+Diego3185006341@users.noreply.github.com>
2024-10-27 20:33:52 -0500

    .

--- src/main/java/com/test/Entity/Employee.java
+++ src/main/java/com/test/Entity/Employee.java
@@ -14,7 +14,7 @@ public class Employee extends Person{
     private String profile_image;
     private double annual_salary;
     public Employee(Long id, String name, Integer age, double employee_salary, String profile_image, double annual_salary) {
-        super(id, name, age); // Call the constructor of the superclass (Person)
+        super(id, name, age);
         this.employee_salary = employee_salary;
         this.profile_image = profile_image;
         this.annual_salary = annual_salary;
