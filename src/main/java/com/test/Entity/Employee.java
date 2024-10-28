c4f85fec62699fcd0426bc04768e76124a8dc34a
Diego <yeogalindo@gmail.com>
2024-10-27 20:36:26 -0500

    Employee: fixing order

--- src/main/java/com/test/Entity/Employee.java
+++ src/main/java/com/test/Entity/Employee.java
@@ -1,6 +1,9 @@
 package com.test.Entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @NoArgsConstructor
@@ -8,16 +11,17 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Setter
 @SuperBuilder
-public class Employee extends Person{
+public class Employee extends Person {
 
     private double employee_salary;
     private String profile_image;
     private double annual_salary;
+
     public Employee(Long id, String name, Integer age, double employee_salary, String profile_image, double annual_salary) {
-        super(id, name, age); // Call the constructor of the superclass (Person)
+        super(id, name, age);
         this.employee_salary = employee_salary;
         this.profile_image = profile_image;
         this.annual_salary = annual_salary;
     }
 
-    }
+}
